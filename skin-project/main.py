0b6ebbfae68cdb8f4cbaf93d6b306c4b9d38df5a
Muyiwa Abidoye <Forevermuyii@gmail.com>
2025-07-16 13:50:09 +0000

    display image

--- skin-project/main.py
+++ skin-project/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI,UploadFile,status,HTTPException
+from fastapi import FastAPI,UploadFile,status,HTTPException,File
 from pydantic import BaseModel
 import logfire
 import uvicorn
 from schema import HomeResponse
+from pathlib import Path
+import shutil
+from fastapi.responses import StreamingResponse
 
 
 
@@ -26,6 +29,28 @@ def root():
     serve as the entrypoint of the whole application.
     """
     return HomeResponse(message="we are live")
+
+
+@app.post(path="/display_image/", tags = ["Images"])
+def display_image(image_file: UploadFile = File(...)):
+    """This endpoint accepts an image file and displays it 
+    to the user.
+    """
+    try:
+        temp_file = Path(f'temp_{image_file.filename}')
+        # write the image to a new object
+        with temp_file.open(mode="wb") as temp_buffer:
+            shutil.copyfileobj(fsrc=image_file.file, fdst=temp_buffer)
+        
+        return StreamingResponse(content = open(temp_file, mode="rb"), media_type="image/png")
+    except Exception as err:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occured while processing the image")
+    finally:
+        temp_file.unlink(missing_ok=True)
+    
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=4500, reload=True)
